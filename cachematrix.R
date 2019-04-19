e9ce63e3847942dd9b6461b5fb7d3fe9597aff94
viswahub <47908736+viswahub@users.noreply.github.com>
2019-04-19 09:22:26 -0400

    Update cachematrix.R
    Add core logic to the function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Make an cache internally the inverse matrix
+## Retrieve in memory copy when exist 
 
-## Write a short comment describing this function
+## Make and cache inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invrs <- NULL
+    set <- function(y) {
+        x <<- y
+        invrs <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(input) invrs <<- input
+    getinverse <- function() invrs
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Retrieve cached matrix if it exists in mem
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    invrs <- x$getinverse()
+    if(!is.null(invrs)) {
+        message("getting cached data.")
+        return(invrs)
+    }
+    mat <- x$get()
+    invrs <- solve(mat)
+    x$setinverse(invrs)
+    invrs
 }
